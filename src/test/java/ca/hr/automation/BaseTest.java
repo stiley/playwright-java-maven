790f8f2dec4b65b3ddfee97252960b09472fa49a
stiley <34474062+stiley@users.noreply.github.com>
2021-09-29 14:22:22 -0400

    Update BaseTest.java

--- src/test/java/ca/hr/automation/BaseTest.java
+++ src/test/java/ca/hr/automation/BaseTest.java
@@ -15,7 +15,7 @@ public class BaseTest {
     public void setUp(){
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
-                .setHeadless(false)
+                .setHeadless(true)
                 .setSlowMo(1000)
         );
 
